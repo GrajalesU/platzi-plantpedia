78f1ca38f487192456d754f8179757da3a076960
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-21 16:26:57 -0500

    Update HomePage to SSG

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,44 @@
 import { getPlantList } from '@api/index'
+import { Authors } from '@components/Authors'
+import { Hero } from '@components/Hero'
 import { Layout } from '@components/Layout'
 import { PlantCollection } from '@components/PlantCollection'
-import { useEffect, useState } from 'react'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import { useEffect } from 'react'
 
 const PLANT_LIMIT = 10
 
-export default function Home() {
-  const [plants, setPlants] = useState<Plant[]>([])
-  useEffect(() => {
-    getPlantList({ limit: PLANT_LIMIT }).then((data) => setPlants(data))
-  }, [])
-  console.log(plants)
+type HomeProps = {
+  plants: Plant[]
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const plants = await getPlantList({ limit: PLANT_LIMIT })
+
+  return {
+    props: {
+      plants,
+    },
+  }
+}
+
+export default function Home({
+  plants,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  useEffect(() => {}, [])
   return (
     <Layout>
-      <PlantCollection plants={plants} variant="square" />
+      <Hero {...plants[0]} className="mb-20" />
+      <Authors className="mb-10" />
+      <PlantCollection
+        plants={plants.slice(1, 3)}
+        variant="vertical"
+        className="mb-24"
+      />
+      <PlantCollection
+        plants={plants.length > 8 ? plants.slice(3, 9) : plants}
+        variant="square"
+      />
     </Layout>
   )
 }
