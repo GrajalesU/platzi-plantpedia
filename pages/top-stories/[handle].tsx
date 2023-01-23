b89130b0034ff43fe48da5b9ae759f37961432e6
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-23 17:04:32 -0500

    Add author detail page

--- pages/top-stories/[handle].tsx
+++ pages/top-stories/[handle].tsx
@@ -1,33 +1,34 @@
-import { getCategoryList, getPlantListByCategory } from '@api/index'
+import { getAuthorList, getPlantListByAuthor } from '@api/index'
 import { Layout } from '@components/Layout'
 import { PlantCollection } from '@components/PlantCollection'
 import { Typography } from '@material-ui/core'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import { useRouter } from 'next/router'
 
-type CategoryProps = {
-  category: Category
+type AuthorDetailProps = {
+  author: Author
   plants: Plant[]
 }
 
-export const getStaticProps: GetStaticProps<CategoryProps> = async ({
+export const getStaticProps: GetStaticProps<AuthorDetailProps> = async ({
   params,
 }) => {
-  const slug = params?.slug
-  if (typeof slug !== 'string') {
+  const handle = params?.handle
+  if (typeof handle !== 'string') {
     return {
       notFound: true,
     }
   }
 
   try {
-    const { category, entries: plants } = await getPlantListByCategory({
-      category: slug,
+    const authors = await getAuthorList()
+    const author = authors.filter((author) => author.handle === handle)[0]
+    const plants = await getPlantListByAuthor({
+      authorId: author.id,
     })
 
     return {
       props: {
-        category,
+        author,
         plants,
       },
     }
@@ -38,55 +39,51 @@ export const getStaticProps: GetStaticProps<CategoryProps> = async ({
   }
 }
 
-type PathType = {
-  params: {
-    slug: string
-  }
-}
-
 export const getStaticPaths = async () => {
-  const categories = await getCategoryList()
-  const paths: PathType[] = categories.map(({ slug }: Category) => ({
+  const authors = await getAuthorList()
+  const paths = authors.map(({ handle }) => ({
     params: {
-      slug,
+      handle,
     },
   }))
+
   return {
     paths,
-    fallback: true,
+    fallback: false,
   }
 }
-
-export default function Category({
-  category,
+export default function AuthorDetail({
+  author,
   plants,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const router = useRouter()
-  if (router.isFallback) {
-    return (
-      <Layout>
-        <div className="flex justify-center items-center min-h-screen">
-          Loading...
-        </div>
-      </Layout>
-    )
-  }
   return (
     <Layout>
-      <div className="relative text-center mb-10">
+      <div className="relative text-center pb-10 mb-10 border-b-2 border-grey-200">
         <div className="opacity-60 inline-block">
-          <img src={category.icon.url} width={600} />
+          <img src={author.photo.url} width={600} />
         </div>
         <div className="text-container absolute">
           <Typography
             variant="h1"
             className="break-words text-left text-6xl sm:text-8xl"
           >
-            {category.title}
+            {author.fullName}
           </Typography>
-      <Typography variant='h5' component='p' className='mt-5 text-left' color='textPrimary'>
-        {category.description}
-      </Typography>
+          <Typography
+            variant="h2"
+            className="mt-5 text-left ml-8"
+            color="textPrimary"
+          >
+            {author.handle}
+          </Typography>
+          <Typography
+            variant="h5"
+            component="p"
+            className="mt-5 text-left ml-10"
+            color="textPrimary"
+          >
+            {author.biography}
+          </Typography>
         </div>
       </div>
       <PlantCollection plants={plants} variant="square" />
