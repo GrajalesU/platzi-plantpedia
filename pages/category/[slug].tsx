87d7481613f4e7ef736e1192d6e31553e64ddeb2
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-23 14:11:00 -0500

    Add dynamic category page

---
+++ pages/category/[slug].tsx
@@ -0,0 +1,120 @@
+import { getCategoryList, getPlantListByCategory } from '@api/index'
+import { Layout } from '@components/Layout'
+import { PlantCollection } from '@components/PlantCollection'
+import { Typography } from '@material-ui/core'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import { useRouter } from 'next/router'
+
+type CategoryProps = {
+  category: Category
+  plants: Plant[]
+}
+
+export const getStaticProps: GetStaticProps<CategoryProps> = async ({
+  params,
+}) => {
+  const slug = params?.slug
+  if (typeof slug !== 'string') {
+    return {
+      notFound: true,
+    }
+  }
+
+  try {
+    const { category, entries: plants } = await getPlantListByCategory({
+      category: slug,
+    })
+
+    return {
+      props: {
+        category,
+        plants,
+      },
+    }
+  } catch (_) {
+    return {
+      notFound: true,
+    }
+  }
+}
+
+type PathType = {
+  params: {
+    slug: string
+  }
+}
+
+export const getStaticPaths = async () => {
+  const categories = await getCategoryList()
+  const paths: PathType[] = categories.map(({ slug }: Category) => ({
+    params: {
+      slug,
+    },
+  }))
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export default function Category({
+  category,
+  plants,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter()
+  console.log(plants)
+  if (router.isFallback) {
+    return (
+      <Layout>
+        <div className="flex justify-center items-center min-h-screen">
+          Loading...
+        </div>
+      </Layout>
+    )
+  }
+  return (
+    <Layout>
+      <div className="relative text-center mb-10">
+        <div className="opacity-60 inline-block">
+          <img src={category.icon.url} width={600} />
+        </div>
+        <div className="text-container absolute">
+          <Typography
+            variant="h1"
+            component="h2"
+            className="break-words text-left text-6xl sm:text-8xl"
+          >
+            {category.title}
+          </Typography>
+      <Typography variant='h5' component='p' className='mt-5 text-left' color='textPrimary'>
+        {category.description}
+      </Typography>
+        </div>
+      </div>
+      <PlantCollection plants={plants} variant="square" />
+
+      <style jsx>{`
+        .text-container {
+          top: 50%;
+          transform: translateY(-50%);
+          max-width: 400px;
+          left: 3vh;
+        }
+
+        @media screen and (min-width: 600px) {
+          .text-container {
+            max-width: 600px;
+            left: 10vh;
+          }
+        }
+
+        @media screen and (min-width: 1300px) {
+          .text-container {
+            max-width: 600px;
+            left: 15vh;
+          }
+        }
+      `}</style>
+    </Layout>
+  )
+}
--- pages/category/index.tsx
+++ pages/category/index.tsx
@@ -30,22 +30,24 @@ export default function Category({
       <Grid container component="ul" spacing={4}>
         {categories.map((category) => (
           <Grid key={category.id} role="listitem" item xs={6} md={4}>
-            <div className="opacity-95 hover:opacity-100">
-              <img src={category.icon.url} alt={category.icon.title} />
-              <Typography variant="h3">{category.title}</Typography>
-              <div className="px-4 py-4">
-                <Typography
-                  variant="body1"
-                  className="my-6"
-                  color="textSecondary"
-                >
-                  {category.description}
-                </Typography>
-                <Link href={`/category/${category.slug}`} passHref>
-                  <Button>Read more</Button>
-                </Link>
+            <Link href={`/category/${category.slug}`}>
+              <div className="opacity-95 hover:opacity-100">
+                <img src={category.icon.url} alt={category.icon.title} />
+                <Typography variant="h3">{category.title}</Typography>
+                <div className="px-4 py-4">
+                  <Typography
+                    variant="body1"
+                    className="my-6"
+                    color="textSecondary"
+                  >
+                    {category.description}
+                  </Typography>
+                  <Link href={`/category/${category.slug}`} passHref>
+                    <Button>Read more</Button>
+                  </Link>
+                </div>
               </div>
-            </div>
+            </Link>
           </Grid>
         ))}
       </Grid>
