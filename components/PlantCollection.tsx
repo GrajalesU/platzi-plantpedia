65c24b72f3d808a1790cbfeb13915653c1d8a6d6
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-24 17:33:53 -0500

    Add memoization

--- components/PlantCollection.tsx
+++ components/PlantCollection.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 
 import { Excerpt } from '@components/Excerpt'
 import Image from '@components/Image'
+import { memo } from 'react'
 
 type PlantCollectionProps = {
   plants: Plant[]
@@ -20,7 +21,7 @@ export function PlantCollection({
   return (
     <Grid container component="ul" spacing={4} className={className}>
       {plants.map((plant) => (
-        <PlantEntry key={plant.id} plant={plant} variant={variant} />
+        <MemoizedPlantEntry key={plant.id} plant={plant} variant={variant} />
       ))}
     </Grid>
   )
@@ -30,6 +31,12 @@ type PlantEntryType = {
   plant: Plant
   variant?: 'square' | 'vertical'
 }
+
+const isEqual = (prevProps: PlantEntryType, newProps: PlantEntryType) => {
+  return prevProps.plant.plantName === newProps.plant.plantName
+}
+
+export const MemoizedPlantEntry = memo(PlantEntry, isEqual)
 
 export function PlantEntry({ plant, variant = 'square' }: PlantEntryType) {
   let gridItemProps: GridProps = { xs: 6, md: 4 }
