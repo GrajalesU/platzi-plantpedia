c8b4b3d98672aa6ebf1987d5e41619ebba874f97
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-24 12:07:44 -0500

    Add top stories Internationalization

--- pages/top-stories/index.tsx
+++ pages/top-stories/index.tsx
@@ -3,29 +3,37 @@ import Image from '@components/Image'
 import { Layout } from '@components/Layout'
 import { Grid, Typography } from '@material-ui/core'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Link from 'next/link'
 
 type AuthorsProps = {
   authors: Author[]
 }
 
-export const getStaticProps: GetStaticProps<AuthorsProps> = async () => {
+export const getStaticProps: GetStaticProps<AuthorsProps> = async ({locale}) => {
   const authors = await getAuthorList()
+  const i18nConf = await serverSideTranslations(locale!)
+
   return {
     props: {
       authors,
     },
     revalidate: 10 * 60,
+    ...i18nConf,
+
   }
 }
 
 export default function Authors({
   authors,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const { t } = useTranslation('common')
+
   return (
     <Layout>
       <Typography variant="h1" className="break-words pb-8 px-4">
-        Authors
+      {t('authors')}
       </Typography>
       <Grid container spacing={4} className="mb-10" justify="center">
         {authors.map(({ id, photo, fullName, handle }) => (
